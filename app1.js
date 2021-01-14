3d61daafbc0c2af7547477fa8eb1d5186de28fa7
rafasarzi <rajustino@gmail.com>
2021-01-14 19:39:58 -0300

    add app1.js (convert fahrenheit)

---
+++ app1.js
@@ -0,0 +1,6 @@
+//perguntar grau em Celsius
+let temperature = prompt("Qual a temperatura em Celsius?")
+//converter celsius para fahrenheit
+let convertToFahrenheit = (temperature * 1.8) + 32
+//mostrar com document.write
+document.write("A temperatura em Fahrenheit é: " + convertToFahrenheit)
