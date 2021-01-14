9bc66d4c0550da259ea06d1e94893167546a4ac5
rafasarzi <rajustino@gmail.com>
2021-01-14 19:42:10 -0300

    age to seconds

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //perguntar a idade pelo prompt...
-let age = prompt("Qual sua idade?")
+let age = parseInt(prompt("Qual sua idade?"))
 //converter idade em segundos...
 let ageInSeconds = age*365*24*60
 //mostrar com document.write
-document.write("Em seus, " + age + "anos se passaram, " + ageInSeconds + " segundos!")
+document.write("Em seus, " + age + "anos, se passaram " + ageInSeconds + " segundos! ")
+
