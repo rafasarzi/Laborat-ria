8a891bebca21adbc91013cb266f6e591fd65277b
rafasarzi <rajustino@gmail.com>
2021-01-14 18:28:40 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+//Estudos guiados Laboratoria
---
+++ app.js
@@ -0,0 +1,6 @@
+//perguntar a idade pelo prompt...
+let age = prompt("Qual sua idade?")
+//converter idade em segundos...
+let ageInSeconds = age*365*24*60
+//mostrar com document.write
+document.write("Em seus, " + age + "anos se passaram, " + ageInSeconds + " segundos!")
